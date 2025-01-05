31a262d4fcb75f692516d6c48f3e63d9ee06b2a4
Victor Maldonado <67714035+vmlcode@users.noreply.github.com>
2025-01-04 21:22:51 -0400

    Add resistance sequence processing and new Jupyter notebook for k-mer model analysis

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,17 @@ import os
 import pandas as pd 
 from utils import ReadFasta, kmer
 
+resistance_sequences = os.listdir("./db/Resistance Sequences")
+
+resistance_df = pd.DataFrame({"seq": [], "AMR": []})
+
+for file in resistance_sequences:
+    read = ReadFasta(f"./db/Resistance Sequences/{file}")
+    resistance_df.loc[len(resistance_df)] = [str(read["seq"]), 1]
+
+resistance_df.to_csv("./db/resistance_amr.csv", index=False)
+print(resistance_df)
+
 sensible_data = os.listdir("./db/WGS/Sensible")
 resistant_data = os.listdir("./db/WGS/Resistant")
 
@@ -22,3 +33,5 @@ for seq in df["wgs"]:
     df["kmer"] = [kmer(seq, 10) for seq in df["wgs"]]
 
 print(df)
+
+df.to_csv("./db/wgs_amr.csv", index=False)
